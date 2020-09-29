685a87c0e7459f6e0c5dfc9f2e662865560fb6a9
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-09-29 15:09:34 +0530

    Add files via upload

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,34 +1,58 @@
 public class EmployeeWageBuilder{
-	public static final int IS_PART_TIME =3;
-	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 2;
-	public static final int MAX_HRS_IN_MONTH = 10;
+	//Global constants
+	static final int PART_TIME = 1;
+	static final int FULL_TIME = 2;
 	
-	public static void main(String[] args) {
-		// Variables
+	//Method to calculate employee wage
+	private static int calculateEmpWage(int emp_wage_per_hr,int max_working_days,int max_hrs_in_month) {
+		//variables
 		int empHrs = 0;
-		int totalEmpHrs = 0;
-		int totalWorkingDays = 0;
-		// Computation
-		while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-			totalWorkingDays++;
-			int empCheck = (int) Math.floor (Math.random() * 10) % 3;
-			switch (empCheck) {
-			case IS_PART_TIME:
-				empHrs = 4;
+		int total_empHrs = 0;
+		int total_working_days = 0;
+		int total_empWage = 0;
+		
+		//computation
+		while(total_empHrs <= max_hrs_in_month && total_working_days < max_working_days) {
+			total_working_days++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			
+			switch(empCheck) {
+			case PART_TIME:
+				empHrs = 8;
 				break;
-			case IS_FULL_TIME:
+			case FULL_TIME:
 				empHrs = 8;
 				break;
 			default:
-			empHrs = 0;
+				empHrs = 0;
+				break;
 			}
 			
-		totalEmpHrs += empHrs;
-		System.out.println("Day#:"+ totalWorkingDays + " Emp Hr:"+ empHrs);
-		int totalEmpwage = totalEmpHrs * EMP_RATE_PER_HOUR; 
-		System.out.println("Total Emp Wage:"+ totalEmpwage);
+			total_empHrs += empHrs;
+			System.out.println("Day : "+total_working_days+" Emp Hrs : "+empHrs );
 		}
+		
+		if(total_empHrs >= max_hrs_in_month)
+			total_empHrs = max_hrs_in_month;															
+		
+		total_empWage = total_empHrs * emp_wage_per_hr;
+				
+		return total_empWage;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee Wage Calculator");
+		
+		//constants
+		int PER_HR_WAGE = 20;
+		int MAX_WORKING_DAYS = 20;
+		int MAX_HRS = 100;
+		//variables
+		int total_empWage = 0;
+		
+		//computation
+		total_empWage = calculateEmpWage(PER_HR_WAGE, MAX_WORKING_DAYS, MAX_HRS);
+		System.out.println("Total Employee Wage : "+total_empWage);
+				
 	}
 }
